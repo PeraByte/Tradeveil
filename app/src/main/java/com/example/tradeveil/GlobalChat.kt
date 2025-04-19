7dee74d172609233d6be2db829dde91287eb4740
Parshant14 <tharwani141208@gmail.com>
2025-04-19 18:48:08 +0500

    Initial comit for backend

---
+++ app/src/main/java/com/example/tradeveil/GlobalChat.kt
@@ -0,0 +1,24 @@
+package com.example.tradeveil
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tradeveil.databinding.ActivityGlobalChatBinding
+
+
+class GlobalChat : AppCompatActivity() {
+    private lateinit var binding: ActivityGlobalChatBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGlobalChatBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tradeveil/VerifySignUp.kt
@@ -0,0 +1,16 @@
+package com.example.tradeveil
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class VerifySignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_verify_sign_up)
+
+    }
+}
